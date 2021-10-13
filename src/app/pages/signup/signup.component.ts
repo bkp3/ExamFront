c763a514ace7e0c00dd0aaa6d05829b8125281c8
Bhola Kumar <bholakumar.7372@gmail.com>
2021-10-13 15:45:10 +0530

    Send user data to server using HttpClient

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-signup',
@@ -17,13 +18,28 @@ export class SignupComponent implements OnInit {
     phone:''
   };
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
 
   formSubmit(){
     console.log(this.user);
+
+    //addUser: userservice
+    this.userService.addUser(this.user).subscribe(
+      (data)=>{
+        //success
+        console.log(data);
+        alert("success");
+        
+      },
+      (error)=>{
+        //error
+        console.log(error);
+        alert("something went wrong!!");
+      }
+    )
     
   }
 
