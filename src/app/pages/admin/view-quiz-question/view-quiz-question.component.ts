af69e9daeeb5cd3ef6f0545f80b779e6e44fd3a8
Bhola Kumar <bholakumar.7372@gmail.com>
2021-10-30 08:30:29 +0530

    Deleting question

--- src/app/pages/admin/view-quiz-question/view-quiz-question.component.ts
+++ src/app/pages/admin/view-quiz-question/view-quiz-question.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { QuestionService } from 'src/app/services/question.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-view-quiz-question',
@@ -9,32 +10,58 @@ import { QuestionService } from 'src/app/services/question.service';
 })
 export class ViewQuizQuestionComponent implements OnInit {
 
-  qId:any;
-  qTitle:any;
-  questions:any;
+  qId: any;
+  qTitle: any;
+  questions: any;
 
-  constructor(private _route:ActivatedRoute,private _question:QuestionService) { }
+  constructor(private _route: ActivatedRoute, private _question: QuestionService) { }
 
   ngOnInit(): void {
-    this.qId=this._route.snapshot.params.qId;
-    this.qTitle=this._route.snapshot.params.title;
-    
+    this.qId = this._route.snapshot.params.qId;
+    this.qTitle = this._route.snapshot.params.title;
+
     this._question.getQuestionsOfQuiz(this.qId).subscribe(
-      (data:any)=>{
+      (data: any) => {
         //success
         console.log(data);
-        
-        this.questions=data;
+
+        this.questions = data;
 
       },
-      (error)=>{
+      (error) => {
         //error
         console.log(error);
-        
+
       }
     )
 
-    
+
   }
+
+  //delete question
+  deleteQuestion(qid: any) {
+    Swal.fire({
+      icon: 'info',
+      showCancelButton: true,
+      confirmButtonText: 'Delete',
+      title: 'Are you sure, want to delete this question?'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        //confirm
+        this._question.deleteQuestion(qid).subscribe(
+          (data) => {
+            //success
+            Swal.fire('Success', 'Question is deleted successfully.', 'success');
+            this.questions = this.questions.filter((q: any) => q.quesId != qid);
+          },
+          (error) => {
+            //error
+            Swal.fire('Error', 'Something went wrong', 'error');
+          }
+        );
+      }
+    })
+  }
+
 
 }
