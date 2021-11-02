407ac5073a7d19fa8ed23da6fd47b037e80484c8
Bhola Kumar <bholakumar.7372@gmail.com>
2021-11-02 14:23:27 +0530

    Displaying ACTIVE Quizzes

--- src/app/pages/user/load-quiz/load-quiz.component.ts
+++ src/app/pages/user/load-quiz/load-quiz.component.ts
@@ -20,7 +20,7 @@ export class LoadQuizComponent implements OnInit {
         this.catId = params.catId;
         if (this.catId == 0) {
           console.log('Load all the quiz');
-          this._quiz.quizzes().subscribe(
+          this._quiz.getActiveQuizzes().subscribe(
             (data: any) => {
               this.quizzes = data;
               console.log(this.quizzes);
@@ -34,7 +34,7 @@ export class LoadQuizComponent implements OnInit {
 
         } else {
           console.log('Load specific quiz');
-          this._quiz.getQuizzesOfCategory(this.catId).subscribe(
+          this._quiz.getActiveQuizzesOfCategory(this.catId).subscribe(
             (data: any) => {
               this.quizzes = data;
             },
--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -38,4 +38,15 @@ export class QuizService {
     return this._http.get(`${baseUrl}/quiz/category/${cid}`);
   }
 
+  //get active quizzes
+  public getActiveQuizzes() {
+    return this._http.get(`${baseUrl}/quiz/active`);
+  }
+
+  //get active quizzes of category
+  public getActiveQuizzesOfCategory(cid: any) {
+    return this._http.get(`${baseUrl}/quiz/category/active/${cid}`);
+  }
+
+
 }
