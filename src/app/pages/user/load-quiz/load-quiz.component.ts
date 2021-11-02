9cf974651f00ca696c6970089986d61540b27f03
Bhola Kumar <bholakumar.7372@gmail.com>
2021-11-02 12:41:38 +0530

    Filtering Quiz

--- src/app/pages/user/load-quiz/load-quiz.component.ts
+++ src/app/pages/user/load-quiz/load-quiz.component.ts
@@ -10,30 +10,43 @@ import { QuizService } from 'src/app/services/quiz.service';
 export class LoadQuizComponent implements OnInit {
 
   catId: any;
-  quizzes:any;
+  quizzes: any;
 
-  constructor(private _route: ActivatedRoute, private _quiz:QuizService) { }
+  constructor(private _route: ActivatedRoute, private _quiz: QuizService) { }
 
   ngOnInit(): void {
-    this.catId = this._route.snapshot.params.catId;
-    if (this.catId == 0) {
-      console.log('Load all the quiz');
-      this._quiz.quizzes().subscribe(
-        (data:any)=>{
-          this.quizzes=data;
-          console.log(this.quizzes);
-          
-        },
-        (error)=>{
-          console.log(error);
-          
+    this._route.params.subscribe(
+      (params: any) => {
+        this.catId = params.catId;
+        if (this.catId == 0) {
+          console.log('Load all the quiz');
+          this._quiz.quizzes().subscribe(
+            (data: any) => {
+              this.quizzes = data;
+              console.log(this.quizzes);
+
+            },
+            (error) => {
+              console.log(error);
+
+            }
+          );
+
+        } else {
+          console.log('Load specific quiz');
+          this._quiz.getQuizzesOfCategory(this.catId).subscribe(
+            (data: any) => {
+              this.quizzes = data;
+            },
+            (error) => {
+              console.log(error);
+
+            }
+          );
         }
-      );
 
-    } else {
-      console.log('Load specific quiz');
-
-    }
+      }
+    );
 
   }
 
--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -33,4 +33,9 @@ export class QuizService {
     return this._http.put(`${baseUrl}/quiz/`, quiz);
   }
 
+  //get quizzes of category
+  public getQuizzesOfCategory(cid: any) {
+    return this._http.get(`${baseUrl}/quiz/category/${cid}`);
+  }
+
 }
